3f599312301e7f599a3e1cd68ec840ec3a7716d8
Marcin Zogrodnik <kontakt@marcinzogrodnik.pl>
2024-05-14 01:19:11 +0200

    git init

---
+++ src/app/new-job/page.tsx
@@ -0,0 +1,12 @@
+export default function NewJob() {
+  return (
+    <>
+      <ul>
+        <li>Basic - $0 / free</li>
+        <li>Featured = $99 / 390 pln - 30 days on top</li>
+        <li>Featured Pro = $199 / 790 pln - Featured + custom logo</li>
+        <li>Featured Pro Max = $299 / 1187 pln - Pro + own highlight</li>
+      </ul>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,138 @@
-import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+type CategoriesProps = {
+  name: string;
+};
+
+type JobPosts = {
+  name: string;
+  company: string;
+  location: string;
+  city: string;
+  categories: string[];
+  salary: string | number | null;
+  isFeatured: boolean;
+};
+
+const categories: CategoriesProps[] = [
+  {
+    name: "All AI jobs",
+  },
+  {
+    name: "Engineering",
+  },
+  {
+    name: "Machine Learning",
+  },
+  {
+    name: "Data",
+  },
+  {
+    name: "Research",
+  },
+  {
+    name: "Design",
+  },
+  {
+    name: "Marketing",
+  },
+  {
+    name: "Sales",
+  },
+  {
+    name: "Finance",
+  },
+];
+
+const jobs: JobPosts[] = [
+  {
+    name: "Lead Developer",
+    company: "Netflix",
+    location: "United Kingdom",
+    city: "London",
+    categories: ["LLM", "finance", "legal"],
+    salary: "20",
+    isFeatured: true,
+  },
+  {
+    name: "Developer",
+    company: "Netflix",
+    location: "United Kingdom",
+    city: "Worksop",
+    categories: ["LLM", "finance", "legal"],
+    salary: null,
+    isFeatured: false,
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <section className="max-w-4xl mx-auto px-5">
+        <nav className="flex justify-between items-center">
+          <p>AI Jobs Hub</p>
+          <Link href="/new-job">Post a job</Link>
+        </nav>
+
+        <div className="flex gap-2 py-10">
+          {categories.map((category) => (
+            <Badge
+              key={category.name.toLowerCase().replace(" ", "-")}
+              className="cursor-pointer"
+              variant="outlineSm"
+            >
+              {category.name}
+            </Badge>
+          ))}
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="flex flex-col gap-5">
+          {jobs.map((job) => (
+            <Link
+              key={job.name.toLowerCase().replace(" ", "-")}
+              href="#"
+              className={cn(
+                "max-w-2xl w-full mx-auto p-3 rounded-xl bg-gray-50 ",
+                job.isFeatured &&
+                  "ring-1 ring-offset-4 ring-gray-300 transition-all hover:ring-offset-0"
+              )}
+            >
+              <p className="text-sm">{job.company}</p>
+              <div className="flex justify-between">
+                <p
+                  className={cn(
+                    "font-medium",
+                    job.isFeatured && "text-lg font-semibold"
+                  )}
+                >
+                  {job.name}
+                </p>
+                {job.salary ? (
+                  <p>${job.salary}k</p>
+                ) : (
+                  <p className="text-sm">Undisclosed salary</p>
+                )}
+                {/* <p>{job.salary ? `$${job.salary}k` : "Undisclosed salary"}</p> */}
+              </div>
+              <div className="flex gap-10">
+                <p className="text-sm">
+                  {job.city}, {job.location}
+                </p>
+                <div className="flex gap-2">
+                  {job.categories.map((category) => (
+                    <Badge variant="outline" key={category}>
+                      #{category}
+                    </Badge>
+                  ))}
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
